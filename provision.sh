8aa7a61538bd13d106006e8c3f075d99ff95f3f4
akase244 <akase244@gmail.com>
2015-06-16 19:25:43 +0900

    run process changed.

--- provision.sh
+++ provision.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# generate rsa key
-mkdir -p ~/.ssh
-if [[ ! -f ~/.ssh/id_rsa ]]; then
-  ssh-keygen -t rsa -q -f ~/.ssh/id_rsa -P ""
-fi
-
 # install epel
 sudo rpm --import http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
@@ -13,6 +7,12 @@ sudo yum localinstall -y epel-release-6-8.noarch.rpm
 
 # install sshpass(noninteractive ssh password provider)
 sudo yum install -y sshpass
+
+# generate rsa key
+mkdir -p ~/.ssh
+if [[ ! -f ~/.ssh/id_rsa ]]; then
+  ssh-keygen -t rsa -q -f ~/.ssh/id_rsa -P ""
+fi
 
 # copy rsa key
 cat ~/.ssh/id_rsa.pub | sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@192.168.33.11 "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
